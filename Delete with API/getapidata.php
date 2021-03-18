ad18fc3915defdd20e89ee240d24b4e9e7803e66
tanmoy sikder <78799835+tanmoysikder39@users.noreply.github.com>
2021-03-18 18:42:06 +0600

    Add files via upload

---
+++ Delete with API/getapidata.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Device;
+use Illuminate\Http\Request;
+
+class getapidata extends Controller
+{
+   function index(Request $request){
+        $device= new Device();
+        $device->name=$request->name;
+        $device->member_id=$request->member_id;
+      
+        $result=$device->save();
+        if($result){
+            return ["result"=>"Data has been save"];
+        }else{
+            return ["result"=>"Data hasnot been save"];
+        }
+    
+   }
+   function update(Request $req){
+       $device= Device::find($req->id);
+       $device->name=$req->name;
+       $device->member_id=$req->member_id;
+       $device->id=$req->id;
+       $result=$device->save();
+       if ($result){
+           return ["result"=>"id updated"];
+       }else{
+        return ["result"=>"id not updated"];
+       }
+   }
+   function delete($id){
+       $device=Device::find($id);
+      $result= $device->delete();
+        if($result){
+            return ["Result"=>"data has been deleted"];
+        }else{
+            return ["Result"=>"data has not been deleted"];
+        }
+
+      
+   }
+}
